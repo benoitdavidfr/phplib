cdc2a065ed270c047791ba8d01968721ec61b1be
Benoit DAVID <benoit.davi@free.fr>
2020-06-18 00:32:17 +0000

    ajout pgsql.inc.php

--- mysql.inc.php
+++ mysql.inc.php
@@ -1,14 +1,23 @@
 <?php
 /*PhpDoc:
 name: mysql.inc.php
-title: mysql.inc.php - classes MySql et MySqlResult utilisées pour exécuter des requêtes MySql
+title: mysql.inc.php - classe MySql simplifiant l'utilisation de MySql
 classes:
 doc: |
-  Simplification de l'utilisation de MySql.
-  La méthode statique MySql::open() prend en paramètre les paramètres MySql
-  sous la forme mysql://{user}:{passwd}@{host}/{database}
-  Voir utilisation en fin de fichier
+  L'ouverture de la connexion s'effectue par la méthode statique MySql::open() qui prend en paramètre les paramètres
+  de connexion MySql sous la forme mysql://{user}:{passwd}@{host}/{database} ou mysql://{user}@{host}/{database}
+  Dans ce dernier cas, le mot de passe doit être enregistré dans le fichier secret.inc.php
+  Le paramètre {database} est optionnel.
+  Sur localhost ou docker, si la base est définie et n'existe pas alors elle est créée. 
+  L'exécution d'une requête Sql s'effectue au travers de la méthode statique MySql::query() qui:
+    - génère une exception en cas d'erreur
+    - pour les requêtes renvoyant un ensemble de n-uplets renvoie un objet MySql pouvant être itéré pour obtenir
+      chacun des n-uplets
+    - pour les autres requêtes renvoie TRUE
 journal: |
+  23/5/2019:
+    ajout possibilité de ne pas fournir le mot de passe
+    ajout création de la base si elle n'existe pas sur localhost ou docker
   26/4/2019:
     restructuration
   3/8/2018 15:00
@@ -17,8 +26,15 @@ journal: |
     création
 */
 
-// la classe MySql implémente en statique une connexion
-// Un objet est créé par résultat d'une requête pour itérer dessus
+/*PhpDoc: classes
+name: MySql
+title: class MySql implements Iterator - classe simplifiant l'utilisation de MySql
+methods:
+doc: |
+  D'une part cette classe implémente en statique la connexion au serveur et l'exécution d'une requête Sql.
+  D'autre part, pour les requêtes renvoyant un ensemble de n-uplets, un objet de la classe est créé
+  qui peut être itéré pour obtenir les n-uplets.
+*/
 class MySql implements Iterator {
   static $mysqli=null; // handle MySQL
   static $server=null; // serveur MySql
@@ -27,21 +43,53 @@ class MySql implements Iterator {
   private $ctuple = null; // le tuple courant ou null
   private $first = true; // vrai ssi aucun rewind n'a été effectué
   
-  // ouvre une connexion MySQL et enregistre le handle en variable statique
-  // Il est nécessaire de passer en paramètre les paramètres MySQL
-  static function open(string $mysqlParams): void {
-    if (!preg_match('!^mysql://([^:]+):([^@]+)@([^/]+)/(.*)$!', $mysqlParams, $matches))
-      throw new Exception("Erreur: dans MySql::open() params \"".$mysqlParams."\" incorrect");
+  /*PhpDoc: methods
+  name: open
+  title: "static function open(string $params): void - ouvre une connexion MySql"
+  doc: |
+    ouvre une connexion MySQL et enregistre le handle en variable statique
+    Il est nécessaire de passer en paramètre les paramètres de connexion MySQL
+    S'ils ne contiennent pas le mot de passe alors ce dernier doit être présent dans le fichier secret.inc.php
+    Si la base est définie et n'existe pas sur localhost ou docker alors elle est créée
+  */
+  static function open(string $params): void {
+    if (!preg_match('!^mysql://([^@]+)@([^/]+)/(.*)$!', $params, $matches))
+      throw new Exception("Erreur: dans MySql::open() params \"".$params."\" incorrect");
     //print_r($matches);
-    self::$mysqli = new mysqli($matches[3], $matches[1], $matches[2], $matches[4]);
-    self::$server = $matches[3];
+    $user = $matches[1]; // "{user}" ou "{user}:{passwd}"
+    $server = $matches[2];
+    $database = $matches[3];
+    if (preg_match('!^([^:]+):(.*)$!', $user, $matches)) { // cas où le mot de passe est passé dans les paramètres
+      $user = $matches[1];
+      $passwd = $matches[2];
+    }
+    elseif (!is_file(__DIR__.'/secret.inc.php'))
+      throw new Exception("Erreur: dans MySql::open($params), fichier secret.inc.php absent");
+    else {
+      $secrets = require(__DIR__.'/secret.inc.php');
+      $passwd = $secrets['sql']["mysql://$user@$server/"] ?? null;
+      if (!$passwd)
+        throw new Exception("Erreur: dans MySql::open($params), mot de passe absent de secret.inc.php");
+    }
+    self::$mysqli = new mysqli($server, $user, $passwd);
+    self::$server = $server;
     // La ligne ci-dessous ne s'affiche pas correctement si le serveur est arrêté !!!
     //    throw new Exception("Connexion MySQL impossible pour $server_name : ".mysqli_connect_error());
     if (mysqli_connect_error())
       throw new Exception("Erreur: dans MySql::open() connexion MySQL impossible sur $mysqlParams");
+    if ($database && (($server == 'localhost') || preg_match('!^172\.17\.0\.[0-9]$!', $server))) {
+      $sql_cr_db = "CREATE DATABASE IF NOT EXISTS `$database` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+      if (!(self::$mysqli->query($sql_cr_db)))
+        throw new Exception ("Requete \"".$sql_cr_db."\" invalide: ".$mysqli->error);
+    }
     if (!self::$mysqli->set_charset ('utf8'))
       throw new Exception("Erreur: dans MySql::open() mysqli->set_charset() impossible : ".self::$mysqli->error);
+    if ($database)
+      if (!self::$mysqli->select_db($database))
+        throw new Exception ("select_db($database) invalide: ".self::$mysqli->error);
   }
+    
+  static function close() { self::$mysqli->close(); }
   
   static function server(): string {
     if (!self::$server)
@@ -49,8 +97,13 @@ class MySql implements Iterator {
     return self::$server;
   }
   
-  // exécute une requête MySQL, soulève une exception en cas d'erreur, renvoie le résultat
-  // soit TRUE soit un itérateur
+  /*PhpDoc: methods
+  name: query
+  title: "static function query(string $sql)- exécute une requête MySQL, soulève une exception ou renvoie le résultat"
+  doc: |
+    exécute une requête MySQL, soulève une exception en cas d'erreur, sinon renvoie le résultat soit TRUE
+    soit un objet MySql qui peut être itéré pour obtenir chaque n-uplet
+  */
   static function query(string $sql) {
     if (!self::$mysqli)
       throw new Exception("Erreur: dans MySql::query() mysqli non défini");
@@ -92,9 +145,12 @@ class MySql implements Iterator {
 
 if (basename(__FILE__)<>basename($_SERVER['PHP_SELF'])) return;
 
-MySql::open('mysql://root:htpqrs28@172.17.0.3/route500');
-$sql = "select *
-from INFORMATION_SCHEMA.TABLES
+
+echo "<pre>";
+//MySql::open('mysql://root:htpqrs28@172.17.0.3/route500');
+//MySql::open('mysql://root@172.17.0.3/route500');
+MySql::open('mysql://root@172.17.0.3/');
+$sql = "select * from INFORMATION_SCHEMA.TABLES
 where table_schema<>'information_schema' and table_schema<>'mysql'";
 if (0) {  // Test 2 rewind 
   $result = MySql::query($sql);
--- sql.inc.php
+++ sql.inc.php
@@ -6,26 +6,40 @@ includes: [ mysql.inc.php ]
 classes:
 doc: |
   Simplification de l'utilisation de MySql et PgSql.
-  La méthode statique Sql::open() prend en paramètre les paramètres MySql ou PgSql
-  sous la forme "mysql://{user}:{passwd}@{host}/{database}"
-  ou 'host=172.17.0.4 dbname=postgres user=postgres password=benoit'
-  Voir utilisation en fin de fichier
+  La méthode statique Sql::open() prend en paramètre les paramètres MySql ou PgSql sous la forme:
+    - "mysql://{user}:{passwd}@{host}/{database}" pour "mysql://{user}@{host}/{database}" pour MySql ou
+    - "host={host} dbname={database} user={user} password={passwd}" ou
+      "host={host} dbname={database} user={user}" pour PgSql
+  Si le mot de passe n'est pas fourni alors il doit être défini dans le fichier secret.inc.php
 journal: |
+  24/5/2019:
+    - correction de PgSql::query()
+    - amélioration de Sql::query()
+  23/5/2019:
+    ajout possibilité de ne pas fournir de mot de passe à la connexion s'il est stocké dans le fichier secret.inc.php
   26/4/2019 17:40
     création
 */
 require_once __DIR__.'/mysql.inc.php';
 
+/*PhpDoc: classes
+name: Sql
+title: class Sql - classe Sql utilisée pour exécuter des requêtes Sql
+methods:
+*/
 class Sql {
   static $software = ''; // 'MySql' ou 'PgSql'
   
+  /*PhpDoc: methods
+  name: sql.inc.php
+  title: "static function open(string $params) - ouverture d'une connexion à un serveur de BD"
+  doc: |
+  */
   static function open(string $params) {
-    if (preg_match('!^mysql://([^:]+):([^@]+)@([^/]+)/(.*)$!', $params)) {
+    if (strncmp($params, 'mysql://', 8) == 0)
       self::$software = 'MySql';
-    }
-    elseif (preg_match('!^host=!', $params)) {
+    elseif (strncmp($params, 'host=', 5) == 0)
       self::$software = 'PgSql';
-    }
     else
       throw new Exception("Erreur: dans Sql::open() params=\"$params\" incorrect");
     (self::$software)::open($params);
@@ -43,10 +57,38 @@ class Sql {
     return (self::$software)::server();
   }
   
-  static function query(string $sql) {
+  static function close(): void {
+    if (!self::$software)
+      throw new Exception('Erreur: dans Sql::close()');
+    (self::$software)::close();
+  }
+  
+  /*PhpDoc: methods
+  name: sql.inc.php
+  title: "static function query($sql) - éxécution d'une requête"
+  doc: |
+    La requête est soit une chaine soit une liste d'éléments,
+    chacun étant une chaine ou un dictionnaire utilisant comme clé l'id du software.
+    Cela permet d'écrire de manière relativement claire des requêtes dépendant du soft.
+    Si la requête échoue alors renvoie une exception
+    sinon si le résultat est un ensemble de n-uplets alors renvoie un objet MySql ou PgSql qui pourra être itéré
+      pour obtenit chacun des n-uplets
+    sinon renvoie TRUE
+  */
+  static function query($sql) {
     if (!self::$software)
       throw new Exception('Erreur: dans Sql::query()');
-    return (self::$software)::query($sql);
+    if (is_string($sql))
+      return (self::$software)::query($sql);
+    elseif (is_array($sql)) {
+      $sqlstr = '';
+      foreach($sql as $sqlelt) // je balaye chaque élt de la requete
+        // si l'élt est une chaine alors je l'utilise sinon j'en prends l'élément corr. au soft courant
+        $sqlstr .= is_string($sqlelt) ? $sqlelt : ($sqlelt[self::$software] ?? '');
+      return (self::$software)::query($sqlstr);
+    }
+    else
+      throw new Exception('Erreur: dans Sql::query()');
   }
 };
 
@@ -61,9 +103,24 @@ class PgSql implements Iterator {
   private $ctuple = false; // le tuple courant ou false
   
   static function open(string $connection_string) {
-    if (!preg_match('!^host=([^ ]+) dbname=([^ ]+) user=([^ ]+) password=([^ ]+)$!', $connection_string, $matches))
+    if (!preg_match('!^host=([^ ]+) dbname=([^ ]+) user=([^ ]+)( password=([^ ]+))?$!', $connection_string, $matches))
       throw new Exception("Erreur: dans PgSql::open() params \"".$connection_string."\" incorrect");
-    self::$server = $matches[1];
+    $server = $matches[1];
+    $database = $matches[2];
+    $user = $matches[3];
+    $passwd = $matches[4] ?? null;
+    self::$server = $server;
+    if (!$passwd) {
+      if (!is_file(__DIR__.'/secret.inc.php'))
+        throw new Exception("Erreur: dans PgSql::open($connection_string), fichier secret.inc.php absent");
+      else {
+        $secrets = require(__DIR__.'/secret.inc.php');
+        $passwd = $secrets['sql']["pgsql://$user@$server/"] ?? null;
+        if (!$passwd)
+          throw new Exception("Erreur: dans PgSql::open($connection_string), mot de passe absent de secret.inc.php");
+      }
+      $connection_string .= " password=$passwd";
+    }
     if (!pg_connect($connection_string))
       throw new Exception('Could not connect: '.pg_last_error());
   }
@@ -74,10 +131,15 @@ class PgSql implements Iterator {
     return self::$server;
   }
   
+  static function close(): void { pg_close(); }
+  
   static function query(string $sql) {
     if (!($result = @pg_query($sql)))
       throw new Exception('Query failed: '.pg_last_error());
-    return new PgSql($sql, $result);
+    if ($result === TRUE)
+      return TRUE;
+    else
+      return new PgSql($sql, $result);
   }
 
   function __construct(string $sql, $result) { $this->sql = $sql; $this->result = $result; $this->first = true; }
@@ -104,8 +166,10 @@ class PgSql implements Iterator {
 
 if (basename(__FILE__)<>basename($_SERVER['PHP_SELF'])) return;
 
+
+echo "<pre>";
 if (0) { // Test MySql
-  MySql::open('mysql://root:htpqrs28@172.17.0.3/route500');
+  MySql::open('mysql://root@172.17.0.3/route500');
   $sql = "select *
     from INFORMATION_SCHEMA.TABLES
     where table_schema<>'information_schema'
@@ -126,8 +190,9 @@ if (0) { // Test MySql
     }
   }
 }
-elseif (0) { // Test PgSql
-  PgSql::open('host=172.17.0.4 dbname=postgres user=postgres password=benoit'); 
+elseif (1) { // Test PgSql
+  //PgSql::open('host=172.17.0.4 dbname=postgres user=postgres password=benoit'); 
+  PgSql::open('host=172.17.0.4 dbname=postgres user=postgres'); 
   $sql = "select * from INFORMATION_SCHEMA.TABLES where table_schema='public'";
   foreach (PgSql::query($sql) as $tuple) {
     echo "tuple="; print_r($tuple);
