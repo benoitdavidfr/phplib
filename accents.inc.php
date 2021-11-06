5794837bf334757846ad797d0c6a358f65dbf8e7
Benoit DAVID <benoit.davi@free.fr>
2021-11-06 14:27:14 +0000

    correction bugs

---
+++ accents.inc.php
@@ -0,0 +1,21 @@
+<?php
+/*PhpDoc:
+name: accents.inc.php
+title: accents.inc.php - suppression des accents d'une chaine de caractères
+classes:
+doc: |
+  source: https://www.tutos.eu/6436
+journal: |
+  11/11/2020:
+    - création
+*/
+
+function supprimeAccents(string $str): string {
+	$search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô',
+    'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î',
+    'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ');
+	$replace = array('A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O',
+    'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i',
+    'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y');
+	return str_replace($search, $replace, $str);
+}
